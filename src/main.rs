694f7c001a0cc49295d22a586a89be2f5d5ae174
zayats1 <zaiats.bogdan@gmail.com>
2024-06-26 11:42:30 +0300

    uart test

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@
 #![no_main]
 
 mod drivers;
+mod protocol;
 
-use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
 use drivers::stepper::{self, *};
@@ -14,15 +14,40 @@ use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
-
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
+use rp_pico::{
+    entry,
+    hal::{
+        clocks::{init_clocks_and_plls, Clock},
+        gpio::{
+            self,
+            bank0::{Gpio0, Gpio1},
+        },
+        pac::{self, interrupt},
+        sio::Sio,
+        uart::{self, DataBits, StopBits, UartConfig},
+        watchdog::Watchdog,
+    },
+    Pins,
 };
+
+use embedded_hal_nb::serial::{Read, Write};
+
+use core::cell::RefCell;
+use critical_section::Mutex;
+
+/// Alias the type for our UART pins to make things clearer.
+type UartPins = (
+    gpio::Pin<Gpio0, gpio::FunctionUart, gpio::PullNone>,
+    gpio::Pin<Gpio1, gpio::FunctionUart, gpio::PullNone>,
+);
+
+/// Alias the type for our UART to make things clearer.
+type Uart = uart::UartPeripheral<uart::Enabled, pac::UART0, UartPins>;
+
+/// This how we transfer the UART into the Interrupt Handler
+static GLOBAL_UART: Mutex<RefCell<Option<Uart>>> = Mutex::new(RefCell::new(None));
+// Time handling traits
+use fugit::RateExtU32;
 
 #[entry]
 fn main() -> ! {
@@ -48,7 +73,7 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let pins = bsp::Pins::new(
+    let pins = Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
@@ -70,6 +95,39 @@ fn main() -> ! {
     let mut stepper = stepper::StepperWithDriver::new(led_pin, dir_pin);
     stepper.set_dir(Direction::Forward);
 
+    let uart_pins = (
+        // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
+        pins.gpio0.reconfigure(),
+        // UART RX (characters received by RP2040) on pin 2 (GPIO1)
+        pins.gpio1.reconfigure(),
+    );
+
+    // Make a UART on the given pins
+    let mut uart = uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(9600.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
+    unsafe {
+        // Enable the UART interrupt in the *Nested Vectored Interrupt
+        // Controller*, which is part of the Cortex-M0+ core.
+        pac::NVIC::unmask(pac::Interrupt::UART0_IRQ);
+    }
+
+    // Tell the UART to raise its interrupt line on the NVIC when the RX FIFO
+    // has data in it.
+    uart.enable_rx_interrupt();
+
+    // Write something to the UART on start-up so we can check the output pin
+    // is wired correctly.
+    uart.write_full_blocking(b"uart_interrupt example started...\n");
+
+    critical_section::with(|cs| {
+        GLOBAL_UART.borrow(cs).replace(Some(uart));
+    });
+
     loop {
         info!("on!");
         stepper.step();
@@ -78,6 +136,39 @@ fn main() -> ! {
         stepper.step();
         delay.delay_ms(500);
     }
+}
+
+#[interrupt]
+#[allow(non_snake_case)]
+fn UART0_IRQ() {
+    // This variable is special. It gets mangled by the `#[interrupt]` macro
+    // into something that we can access without the `unsafe` keyword. It can
+    // do this because this function cannot be called re-entrantly. We know
+    // this because the function's 'real' name is unknown, and hence it cannot
+    // be called from the main thread. We also know that the NVIC will not
+    // re-entrantly call an interrupt.
+    static mut UART: Option<uart::UartPeripheral<uart::Enabled, pac::UART0, UartPins>> = None;
+
+    // This is one-time lazy initialisation. We steal the variable given to us
+    // via `GLOBAL_UART`.
+    if UART.is_none() {
+        critical_section::with(|cs| {
+            *UART = GLOBAL_UART.borrow(cs).take();
+        });
+    }
+
+    // Check if we have a UART to work with
+    if let Some(uart) = UART {
+        // Echo the input back to the output until the FIFO is empty. Reading
+        // from the UART should also clear the UART interrupt flag.
+        while let Ok(byte) = uart.read() {
+            let _ = uart.write(byte);
+        }
+    }
+
+    // Set an event to ensure the main thread always wakes up, even if it's in
+    // the process of going to sleep.
+    cortex_m::asm::sev();
 }
 
 // End of file
