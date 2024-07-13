e5d2e234440ca6ecd20872cb2ed33a802ea973d7
zayats1 <zaiats.bogdan@gmail.com>
2024-07-13 20:45:15 +0300

    improve parser

--- src/protocol/parser.rs
+++ src/protocol/parser.rs
@@ -8,7 +8,11 @@ use super::{
 };
 
 #[derive(PartialEq)]
-pub struct ParsingError;
+pub enum ParsingError {
+    SepparatorError,
+    ValueParsingError,
+    NotAComandError,
+}
 
 impl fmt::Display for ParsingError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -17,38 +21,48 @@ impl fmt::Display for ParsingError {
 }
 
 pub fn parse(signal: &str) -> Result<Message, ParsingError> {
-    if let Some(sep_idx) = get_sepparator_index(signal, SEPPARATOR) {
-        let cut_part_1 = &signal[..sep_idx];
+    let Some(sep_idx) = get_sepparator_index(signal, SEPPARATOR) else {
+        return Err(ParsingError::SepparatorError);
+    };
+    let cut_part_1 = &signal[..sep_idx];
 
-        if let Some(val_sep_idx) = get_sepparator_index(cut_part_1, EQ_VAL) {
-            let comand = &cut_part_1[..val_sep_idx];
-            let value = &cut_part_1[val_sep_idx + 1..];
+    let Some(val_sep_idx) = get_sepparator_index(cut_part_1, EQ_VAL) else {
+        return Err(ParsingError::SepparatorError);
+    };
 
-            match comand {
-                ANGLE_PREFIX => {
-                    if let Some(angle) = value.parse::<f32>().ok() {
-                        return Ok(Message::ServoAngle(angle));
-                    }
-                }
-                STEPPER_STEPS_PREFIX => {
-                    if let Some(steps) = value.parse::<i32>().ok() {
-                        return Ok(Message::StepperMotorRunSteps(steps));
-                    }
-                }
-                STEPPER_STOP => {
-                    return Ok(Message::StepperStop);
-                }
+    let comand = &cut_part_1[..val_sep_idx];
+    let value = &cut_part_1[val_sep_idx + 1..];
 
-                STEPPER_SPEED_PREFIX => {
-                    if let Some(speed) = value.parse::<f32>().ok() {
-                        return Ok(Message::StepperMotorSpeed(speed));
-                    }
-                }
-                _ => (),
+    match comand {
+        ANGLE_PREFIX => {
+            if let Ok(angle) = value.parse::<f32>() {
+                return Ok(Message::ServoAngle(angle));
+            } else {
+                return Err(ParsingError::ValueParsingError);
             }
         }
+        STEPPER_STEPS_PREFIX => {
+            if let Ok(steps) = value.parse::<i32>() {
+                return Ok(Message::StepperMotorRunSteps(steps));
+            } else {
+                return Err(ParsingError::ValueParsingError);
+            }
+        }
+
+        STEPPER_SPEED_PREFIX => {
+            if let Ok(speed) = value.parse::<f32>() {
+                return Ok(Message::StepperMotorSpeed(speed));
+            } else {
+                return Err(ParsingError::ValueParsingError);
+            }
+        }
+        STEPPER_STOP => {
+            return Ok(Message::StepperStop);
+        }
+        _ => {
+            return Err(ParsingError::NotAComandError);
+        }
     }
-    Err(ParsingError)
 }
 
 fn get_sepparator_index(string: &str, sepparator: char) -> Option<usize> {
