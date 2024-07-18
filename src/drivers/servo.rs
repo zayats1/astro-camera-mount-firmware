05f0a4f641802a126942aff30146a2884d04bdbd
zayats1 <zaiats.bogdan@gmail.com>
2024-07-18 18:30:37 +0300

    fixed servo angle calculation

--- src/drivers/servo.rs
+++ src/drivers/servo.rs
@@ -8,8 +8,8 @@ pub struct Servo<T: SetDutyCycle> {
 
 impl<T: SetDutyCycle> Servo<T> {
     pub fn new(pwm_out: T, period: u8, max_angle: f32) -> Self {
-        let duty_on_zero = u16::MAX / period as u16; // servo pulse range
-        let duty_on_90 = duty_on_zero / 2 * 3;
+        let duty_on_zero = u16::MAX / (period * 2) as u16; // servo pulse range
+        let duty_on_90 = duty_on_zero * 3;
         let duty_per_degree = (duty_on_90 - duty_on_zero) as f32 / 90.0;
         Self {
             pwm_out,
