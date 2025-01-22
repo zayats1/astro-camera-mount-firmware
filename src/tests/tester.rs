f8596876f7d85e1b00994cc3ef8b87e79c9cea45
zayats1 <zaiats.bogdan@gmail.com>
2025-01-22 16:24:37 +0200

    it now builds

--- src/tests/tester.rs
+++ src/tests/tester.rs
@@ -1,5 +1,4 @@
 use core::fmt::Write;
-use function_name::named;
 
 use crate::protocol::{
     message::Message,
@@ -17,16 +16,15 @@ where
     pub fn new(tx: &'a mut T) -> Self {
         Self { tx }
     }
-    #[named]
+
     fn parse_servo_test(&mut self) {
         let message = "ANGLE:90,";
         let res = parse(message);
         self.assert_eq(res, Ok(Message::ServoAngle(90.0)));
 
-        writeln!(&mut self.tx, "{}: PASSED", function_name!()).unwrap();
+        writeln!(&mut self.tx, "parse_servo_test: PASSED").unwrap();
     }
 
-    #[named]
     fn parse_stepper_test(&mut self) {
         let message = "STEPS:25,";
         let res = parse(message);
@@ -36,26 +34,24 @@ where
         let res = parse(message);
         self.assert_eq(res, Ok(Message::StepperStop));
 
-        writeln!(&mut self.tx, "{}: PASSED", function_name!()).unwrap();
+        writeln!(&mut self.tx, "parse_stepper_test: PASSED").unwrap();
     }
 
-    #[named]
     fn parse_value_error_test(&mut self) {
         let message = "STEPS:,";
         let res = parse(message);
         self.assert_eq(res, Err(ParsingError::ValueCanNotBeParsed));
-        writeln!(&mut self.tx, "{}: PASSED", function_name!()).unwrap();
+        writeln!(&mut self.tx, "parse_value_error_tes: PASSED").unwrap();
     }
-    #[named]
+
     fn parse_not_a_comand_error_test(&mut self) {
         let message = "STO:,";
         let res = parse(message);
         self.assert_eq(res, Err(ParsingError::NotAComand));
 
-        writeln!(&mut self.tx, "{}: PASSED", function_name!()).unwrap();
+        writeln!(&mut self.tx, "parse_not_a_comand_error_test: PASSED").unwrap();
     }
 
-    #[named]
     fn parse_sepparator_error_test(&mut self) {
         let message = "STEPS4,";
         let res = parse(message);
@@ -65,7 +61,7 @@ where
         let res = parse(message);
         self.assert_eq(res, Err(ParsingError::NoSepparator));
 
-        writeln!(&mut self.tx, "{}: PASSED", function_name!()).unwrap();
+        writeln!(&mut self.tx, "parse_sepparator_error_test: PASSED").unwrap();
     }
 
     fn assert_eq<U: PartialEq>(&mut self, res: U, expected: U) {
